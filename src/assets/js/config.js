83bf9eb065581ad2d62e750a63b3c411f42aed48
tuur.delacroix <tuur.delacroix@student.howest.be>
2022-03-29 13:23:32 +0200

    fixed some more sonarqube errors in js file

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -4,4 +4,4 @@ const _config = {
     groupnumber: '00',
     errorHandlerSelector: '.errormessages p',
     getAPIUrl: function() { return `https://project-i.ti.howest.be/monopoly-${this.groupnumber}/api`;}
-}
+};

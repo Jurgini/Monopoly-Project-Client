7d43ab50dee8b29b5e9e13e741461ca16cf86ff7
joachim.gielen <joachim.gielen@student.howest.be>
2022-03-30 12:06:20 +0200

    Choosing pawns style selection

---
+++ src/assets/js/game-configuration/pawn-selection.js
@@ -0,0 +1,18 @@
+"use strict";
+document.addEventListener("DOMContentLoaded",init);
+
+function init() {
+    document.querySelectorAll("figure").forEach(figure => {
+        figure.addEventListener("click",choosePawn)
+    });
+
+
+}
+
+function choosePawn(e) {
+    document.querySelectorAll(".chosen").forEach(o =>{
+        o.classList.remove("chosen");
+    });
+    e.target.closest("figure").classList.add("chosen");
+
+}

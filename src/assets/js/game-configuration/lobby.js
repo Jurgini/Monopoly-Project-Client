a66ff77d08243faf6233df6c6c782a0ae1f52f03
brian <brian.bota@student.howest.be>
2022-03-30 16:40:29 +0200

    Added the ability to see players in the lobby screen using the API

--- src/assets/js/game-configuration/lobby.js
+++ src/assets/js/game-configuration/lobby.js
@@ -3,13 +3,33 @@ function initLobby()
     let lobbyId = 'group18_288';
 
     displayLobbyId(lobbyId);
+    loadPlayers(lobbyId)
 }
 
 function displayLobbyId(lobbyId)
 {
     fetchFromServer(`/games?prefix=${lobbyId}`,'GET').then(response => {
       document.querySelector("span#gameid").innerHTML=response[0].id;
-    })
+    });
+}
+function loadPlayers(lobbyId)
+{
+    $container = document.querySelector('div.players');
+    fetchFromServer(`/games?prefix=${lobbyId}`,'GET').then(response => {
+        showPlayers(response[0].players, $container);
+    });
+}
+function showPlayers(playersInGame,$container)
+{
+    console.log(playersInGame)
+    playersInGame.forEach(player => {
+        showPlayer(player,$container);
+    });
+}
+function showPlayer(player, $container)
+{
+    $template = document.querySelector('template').content.firstElementChild.cloneNode(true);
+    $template.querySelector('span.playername').textContent = player.name;
+    $container.insertAdjacentHTML("beforeend", $template.outerHTML);
 
 }
-

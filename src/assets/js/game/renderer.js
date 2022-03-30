5c8aa52927cf2de6f7bbb0f4607a01cce4724ce6
tuur.delacroix <tuur.delacroix@student.howest.be>
2022-03-30 16:03:15 +0200

    added the feature that loads the not yet started games (lobbies), so that players can join them.

--- src/assets/js/game/renderer.js
+++ src/assets/js/game/renderer.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function loadAvailableLobbies()
+{
+    const $container = document.querySelector('div#lobby-overview');
+    fetchFromServer(`/games?started=false&prefix=${_config.gameprefix}`, 'GET')
+        .then(availableGames =>
+        {
+            renderAvailableLobbies(availableGames, $container);
+        });
+}
+
+function renderAvailableLobbies(availableGames, $container)
+{
+    availableGames.forEach(availableGame => {
+        renderLobby(availableGame, $container);
+    });
+}
+
+function renderLobby(availableGame, $container)
+{
+    const $template = document.querySelector('template').content.firstElementChild.cloneNode(true);
+    $template.querySelector("span#data-lobby-name").textContent = availableGame.id;
+    $template.setAttribute("data-lobbyid", availableGame.id);
+    $template.querySelector("span#data-lobby-joined-currently").textContent = availableGame.players.length;
+    $template.querySelector("span#data-lobby-max").textContent = availableGame.numberOfPlayers;
+
+    $container.insertAdjacentHTML('beforeend', $template.outerHTML);
+}

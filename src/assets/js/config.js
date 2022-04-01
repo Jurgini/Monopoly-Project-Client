a27cc6aa59816b50df2b90d152abdb5d56e89f84
brian <brian.bota@student.howest.be>
2022-04-01 15:47:29 +0200

    Added delay to the config file and finalized styling and html of the lobby

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -7,5 +7,6 @@ const _config = {
     localStorageTokenObject: "token",
     errorHandlerSelector: '.errormessages p',
     monopolyUrl: `https://project-i.ti.howest.be/monopoly-18/`,
+    delay: 1500,
     getAPIUrl: function() { return `https://project-i.ti.howest.be/monopoly-${this.groupnumber}/api`;}
 };

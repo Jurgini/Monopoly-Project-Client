ec81dfb3a1f1ea3ecb0b7aeb029aa08fcf4310d3
tuur.delacroix <tuur.delacroix@student.howest.be>
2022-04-01 18:09:28 +0200

    organised file structure

--- src/assets/js/initialiser.js
+++ src/assets/js/initialiser.js
@@ -2,22 +2,8 @@
 
 document.addEventListener('DOMContentLoaded',init);
 
-function init(){
-    testConnection();
-    loadGames();
+function init() {
     loadAvailableLobbies();
     document.querySelector('section#join-lobby-container form').addEventListener('submit', joinSelectedLobby);
     document.querySelector('section#create-game-container form').addEventListener('submit', createGame);
-}
-
-function testConnection(){
-    fetchFromServer('/tiles','GET').then(tiles => console.log(tiles)).catch(errorHandler);
-}
-
-// TO DELETE, ONLY FOR TESTING PURPOSES
-function loadGames()
-{
-    fetchFromServer('/games?prefix=group18', "GET").then(response => {
-        console.log(response);
-    });
 }

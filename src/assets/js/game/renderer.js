3af5a24c9de2e2ae1b67f772c8b258605d6b9157
tuur.delacroix <tuur.delacroix@student.howest.be>
2022-04-04 22:49:43 +0200

    created infinite refresh of the lobby overview, counters change upon join and lobby dissapears when started.

--- src/assets/js/game/renderer.js
+++ src/assets/js/game/renderer.js
@@ -7,11 +7,17 @@ function loadAvailableLobbies()
         .then(availableGames =>
         {
             renderAvailableLobbies(availableGames, $container);
+
+            if (true) // Infinite refresh of lobbies
+            {
+                setTimeout(() => loadAvailableLobbies(), 3500);
+            }
         });
 }
 
 function renderAvailableLobbies(availableGames, $container)
 {
+    $container.innerHTML = $container.querySelector("template").outerHTML;
     availableGames.forEach(availableGame => {
         renderLobby(availableGame, $container);
     });

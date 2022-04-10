7c30be9a5933ef4f55cfdc6fc5f09ae23812e30e
tuur.delacroix <tuur.delacroix@student.howest.be>
2022-04-10 21:49:53 +0200

    players on bottom of the gamescreen their pawn, name and moneybalance is loading with the API. (properties and refreshes need to be implemented later)

--- src/assets/js/game/load-game.js
+++ src/assets/js/game/load-game.js
@@ -7,5 +7,37 @@ function getGameDetails()
     fetchFromServer(`/games/${loadFromStorage('game').gameId}`, 'GET')
         .then(onGoingGame => {
             console.log(onGoingGame);
+            const players = onGoingGame.players;
+            renderPlayersInfo(players);
         });
+}
+
+function renderPlayersInfo(playersInOnGoingGame)
+{
+    playersInOnGoingGame.forEach(player => {
+        const $container = document.querySelector('div#players-container');
+        const playerPawns = loadFromStorage('pawns');
+        let playerPawn;
+        playerPawns.forEach(distribution =>
+        {
+            if (distribution.player === player.name)
+            {
+                playerPawn = distribution.pawn;
+            }
+        });
+
+        renderPlayerInfo(player, playerPawn, $container);
+    });
+}
+
+function renderPlayerInfo(playerInOnGoingGame, playerPawn, $container)
+{
+    const $template = document.querySelector('template#player-info-template').content.firstElementChild.cloneNode(true);
+    const $pawn = $template.querySelector('img');
+    // TODO: If this is the turn taking player add class: player-taking-turn
+    $pawn.setAttribute('src', `images/pawns/${playerPawn.id}.png`);
+    $pawn.setAttribute('alt', playerPawn.displayName);
+    $pawn.setAttribute('title', playerPawn.displayName);
+    $template.querySelector('.player-balance').textContent = `${playerInOnGoingGame.name}: €${playerInOnGoingGame.money}`;
+    $container.insertAdjacentHTML('beforeend', $template.outerHTML);
 }

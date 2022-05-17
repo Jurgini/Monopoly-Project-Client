87a11c1aec6048267755027bbf0e0186e48b9190
TuurDelacroix <tuur.delacroix@proximus.be>
2022-05-17 15:42:15 +0200

    activated lobbyname input

--- src/assets/js/game/create-game.js
+++ src/assets/js/game/create-game.js
@@ -4,13 +4,12 @@ function createGame(e)
 {
     e.preventDefault();
 
-    // Reactivate when we have own server
-    //const lobbyName = document.querySelector('form#create-game-container #lobby-name').value;
+    const $lobbyName = document.querySelector('#lobby-name').value;
     const $playerName = document.querySelector('section#game-settings-container input#username').value;
     const $numberOfPlayers = document.querySelector('section#create-game-container input#amount-of-players').value;
 
     const gameBody = {
-        "prefix": _config.gameprefix, // TODO: Enable this again when we use our own server (check on name before '.' not '_'+ `.${lobbyName}`,
+        "prefix": $lobbyName,
         "numberOfPlayers": parseInt($numberOfPlayers)
     };
 
@@ -18,7 +17,6 @@ function createGame(e)
     {
         fetchFromServer('/games', "POST", gameBody)
             .then(response => {
-                // Use response to send the user to his created lobby
                 joinGame($playerName, response.id);
             });
     }

d38953a947b418255f15e618765fd7e39fdf2684
jurgen-telegrafi <jurgen.telegrafi@student.howest.be>
2022-05-22 15:27:46 +0200

    attempt of fixing automatic bankruptcy (failed but maybe not)

--- src/assets/js/game/game-logic/end-game.js
+++ src/assets/js/game/game-logic/end-game.js
@@ -5,28 +5,42 @@ loadTokenFromStorage();
 function checkGame() {
     fetchFromServer(`/games/${loadFromStorage(_config.localStorageGameObject).gameId}`, "GET")
         .then(onGoingGame => {
-            if (onGoingGame.ended === false)
-            {
-                setTimeout(checkGame, _config.delay);
-            }
-            else
-            {
-                saveToStorage("gameWinner", onGoingGame.winner);
+            if (onGoingGame.ended === false) {
+
                 let bankruptPlayer = onGoingGame.players.filter(player => player.bankrupt === true);
+                console.log(bankruptPlayer);
                 onGoingGame.players.forEach(player => {
-                    if(player.name !== bankruptPlayer.name){
-                        console.log("go to endgame");
-                        endGame();
+                    if (player.name === bankruptPlayer.name) {
+                        console.log("go to bankrupot");
+                        goBankrupt();
 
                     }
                 });
-                goBankrupt();
-                console.log("go to bankrupot");
+
+                setTimeout(checkGame, _config.delay);
+
+            }
+
+            else {
+
+                saveToStorage("gameWinner", onGoingGame.winner);
+                let bankruptPlayer = onGoingGame.players.filter(player => player.bankrupt === true);
+                console.log(bankruptPlayer);
+                onGoingGame.players.forEach(player => {
+                    if (player.name === bankruptPlayer.name) {
+                        console.log("go to bankrupot");
+                        goBankrupt();
+                    }
+                });
+
+                console.log("go to endgame");
+                endGame();
             }
         });
 
 }
-function endGame(){
+
+function endGame() {
     clearGameFromLocalStorage();
     redirect("game-over.html");
 }
